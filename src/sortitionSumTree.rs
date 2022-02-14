555a89f6fabefbf9d85705ba25b3c900efa47cdd
okita-haruru <okitaharuru@gmail.com>
2022-02-14 09:39:49 +0800

    add notes for sortitionTree.rs

--- src/sortitionSumTree.rs
+++ src/sortitionSumTree.rs
@@ -23,11 +23,24 @@ struct SortitionSumTrees {
    sortitionSumTrees: HashMap<TypeKey,SortitionSumTree>,
 }
 impl SortitionSumTrees{
+    /**
+     *  @dev Create a sortition sum tree with a key.
+     *  @param _key The key of the new tree.
+     *  @param _K The max number of children for each node in the new tree.
+     */
     pub fn createTree(&mut self,_key:TypeKey,_K:u128){
         let mut tree: SortitionSumTree = SortitionSumTree::new(_K);
         tree.nodes.push(0);
         self.sortitionSumTrees.insert(_key, tree);
     }
+
+      /**
+     *  @dev Update the parents of a node until root.
+     *  @param _key The key of the tree to update.
+     *  @param _treeIndex The index of the node to start from.
+     *  @param _plusOrMinus Wether to add (true) or substract (false).
+     *  @param _value The value to add or substract.
+     */
     pub fn updateParents(&mut self,_key:TypeKey,_treeIndex:u128,_plusOrMinus:bool,_value:u128){
         if let Some(tree)=self.sortitionSumTrees.get_mut(&_key){
             let mut parentIndex=_treeIndex;
@@ -37,34 +50,44 @@ impl SortitionSumTrees{
             }
         }
     }
+
+     /**
+     *  @dev Set a value of an address in a tree.
+     *  @param _key The key of the tree.
+     *  @param _value The new value.
+     *  @param _ID The ID of the value.
+     *  `O(log_k(n))` where
+     *  `k` is the maximum number of childs per node in the tree,
+     *   and `n` is the maximum number of nodes ever appended.
+     */
     pub fn set(&mut self,_key:TypeKey,_value:u128,_ID:TypeAddress){
         if let Some(tree)=self.sortitionSumTrees.get_mut(&_key){
-            if let Some(_treeIndex)=tree.IDsToNodeIndexes.get_mut(&_ID){
+            if let Some(_treeIndex)=tree.IDsToNodeIndexes.get_mut(&_ID){//node exist
                 let treeIndex=_treeIndex.clone();
-                if (_value == 0) {
+                if (_value == 0) {//new value==0
+                    //remove
                     let value = tree.nodes[treeIndex as usize];
                     tree.nodes[treeIndex.clone() as usize] = 0;
                     tree.stack.push(treeIndex);
                     tree.nodeIndexesToIDs.remove(&treeIndex);
                     let y=treeIndex;
                     tree.IDsToNodeIndexes.remove(&_ID);
-    
                     self.updateParents( _key, treeIndex, false, value);
-                } else if (_value != tree.nodes[treeIndex as usize]) { // New, non zero value.
+                } else if (_value != tree.nodes[treeIndex as usize]) { // New value,and!=0
                     // Set.
                     let plusOrMinus = tree.nodes[treeIndex as usize] <= _value;
                     let plusOrMinusValue:u128 = if plusOrMinus {_value - tree.nodes[treeIndex.clone() as usize]} else {tree.nodes[treeIndex.clone() as usize] - _value};
                     tree.nodes[treeIndex as usize] = _value;
-                    //updateParents(self, _key, treeIndex, plusOrMinus, plusOrMinusValue);
                     self.updateParents(_key, treeIndex, plusOrMinus, plusOrMinusValue);
                 }   
             }else {
-                if (_value != 0) {
+                if (_value != 0) {//node not exist
                     let mut treeIndex:u128=0;
-                    if (tree.stack.len() == 0) {
+                    if (tree.stack.len() == 0) {//no vacant node
                         treeIndex = tree.nodes.len() as u128;
                         tree.nodes.push(_value);
-                        if (treeIndex != 1 && (treeIndex - 1) % tree.K == 0) {
+                        if (treeIndex != 1 && (treeIndex - 1) % tree.K == 0) {//is the first node of a layer
+                            //move the parent  down
                             let parentIndex = treeIndex / tree.K;
                             let parentID : TypeAddress= tree.nodeIndexesToIDs[&parentIndex];
                             let newIndex:u128= treeIndex + 1;
@@ -73,7 +96,7 @@ impl SortitionSumTrees{
                             tree.IDsToNodeIndexes.insert(parentID,newIndex);
                             tree.nodeIndexesToIDs.insert(newIndex,parentID);
                         }
-                    } else {
+                    } else {//vacant node
                         treeIndex = tree.stack[tree.stack.len() - 1];
                         tree.stack.pop();
                         tree.nodes[treeIndex as usize] = _value;
@@ -86,6 +109,12 @@ impl SortitionSumTrees{
             }
         }
     }
+
+    /** @dev Gets a specified ID's associated value.
+     *  @param _key The key of the tree.
+     *  @param _ID The ID of the value.
+     *  @return value The associated value.
+     */
     pub fn stakeOf(&self,_key:TypeKey,_ID:TypeAddress)->u128{
         if let Some(tree)=self.sortitionSumTrees.get(&_key){
             if let Some(treeIndex)=tree.IDsToNodeIndexes.get(&_ID){
@@ -97,6 +126,16 @@ impl SortitionSumTrees{
             return 0;
         }
     }
+
+    /**
+     *  @dev Draw an ID from a tree using a number. Note that this function reverts if the sum of all values in the tree is 0.
+     *  @param _key The key of the tree.
+     *  @param _drawnNumber The drawn number.
+     *  @return ID The drawn ID.
+     *  `O(k * log_k(n))` where
+     *  `k` is the maximum number of childs per node in the tree,
+     *   and `n` is the maximum number of nodes ever appended.
+     */
     pub fn draw(&self,_key:TypeKey,_drawnNumber:u128)->TypeAddress{
         if let Some(tree)=self.sortitionSumTrees.get(&_key){
             let mut treeIndex:u128=0;
@@ -118,6 +157,18 @@ impl SortitionSumTrees{
             return 0;
         }
     }
+
+    /**
+     *  @dev Query the leaves of a tree. Note that if `startIndex == 0`, the tree is empty and the root node will be returned.
+     *  @param _key The key of the tree to get the leaves from.
+     *  @param _cursor The pagination cursor.
+     *  @param _count The number of items to return.
+     *  @return startIndex The index at which leaves start.
+     *  @return values The values of the returned leaves.
+     *  @return hasMore Whether there are more for pagination.
+     *  `O(n)` where
+     *  `n` is the maximum number of nodes ever appended.
+     */
     pub fn queryLeaves(&self,_key:TypeKey,_cursor:u128,_count:u128)->(u128,Vec<u128>,bool){
         let mut startIndex:u128=0;
         let mut values:Vec<u128>=Vec::new();
@@ -138,11 +189,8 @@ impl SortitionSumTrees{
                     break;
                 }
             }
-
         }
         return(startIndex,values,hasMore);
-
     }
-    
 }
 
